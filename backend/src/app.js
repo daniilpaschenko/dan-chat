2dd7263724148b67af51d71cc20b3cdf711bfb60
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-07-17 11:34:26 +0700

    feat(auth): подключить authRoutes и userRoutes в app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
 const app = express();
 
 app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 
 module.exports = app;
