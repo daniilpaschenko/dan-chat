e277543a4573be2d2b5cb5c0f7abe9c8d9d311e1
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-07-18 09:05:56 +0700

    feat(auth): подключить register и login лимитеры в routes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const { register, login } = require('../controllers/authController');
 const authMiddleware = require('../middlewares/auth'); // для защищённых роутов
+const {loginLimiter, registerLimiter} = require('../middlewares/rateLimiters'); // подключение лимитеров
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', registerLimiter, register);
+router.post('/login', loginLimiter, login);
 
 module.exports = router;
