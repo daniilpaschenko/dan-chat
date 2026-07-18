b979cb75e6cc9a53e9e1b3c53c9f99f816df05a1
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-07-18 10:52:50 +0700

    feat(route): /refresh в authRoutes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-const { register, login } = require('../controllers/authController');
+const { register, login, refresh } = require('../controllers/authController');
 const authMiddleware = require('../middlewares/auth'); // для защищённых роутов
 const {loginLimiter, registerLimiter} = require('../middlewares/rateLimiters'); // подключение лимитеров
 
 router.post('/register', registerLimiter, register);
 router.post('/login', loginLimiter, login);
+router.post('/refresh', refresh);
 
 module.exports = router;
