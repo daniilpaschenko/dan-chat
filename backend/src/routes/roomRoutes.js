8bc1feb354f285265d27dfbdcb4f8b0a072156a5
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-07-20 10:58:23 +0700

    feat(routes): добавить руты для сообщений

--- backend/src/routes/roomRoutes.js
+++ backend/src/routes/roomRoutes.js
@@ -9,6 +9,7 @@ const {
     addParticipant,
     removeParticipant,
 } = require('../controllers/roomController');
+const { getRoomMessages, createMessage } = require('../controllers/messageController');
 
 
 router.use(authMiddleware);
@@ -19,5 +20,8 @@ router.get('/:roomId', getRoomById);
 router.post('/:roomId/read', markRoomAsRead);
 router.post('/:roomId/participants', addParticipant);
 router.delete('/:roomId/participants/:userId', removeParticipant);
+
+router.get('/:roomId/messages', getRoomMessages);
+router.post('/:roomId/messages', createMessage);
 
 module.exports = router;
