5901b4220f9795a90fe7fa5b79cc6f95c400a6ca
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-07-20 09:33:07 +0700

    feat: подлкючить roomRoutes в app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
+const roomRoutes = require('./routes/roomRoutes');
 const { globalLimiter } = require('./middlewares/rateLimiters');
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 app.use('/api', globalLimiter); // общая защита на все API-роуты
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/rooms', roomRoutes);
 
 module.exports = app;
