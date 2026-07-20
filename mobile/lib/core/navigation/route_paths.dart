d3b059801200320375109882006c9e9c73aa4185
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-07-20 21:51:00 +0700

    feat: создать файл с роутами

---
+++ mobile/lib/core/navigation/route_paths.dart
@@ -0,0 +1,10 @@
+class RoutePaths {
+  RoutePaths._();
+
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String chatList = '/chats';
+  static const String chatRoom = '/chats/:roomId';
+
+  static String chatRoomPath(String roomId) => '/chats/$roomId';
+}
