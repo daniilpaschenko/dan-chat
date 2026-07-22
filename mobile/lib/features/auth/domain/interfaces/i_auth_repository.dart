0f8f9f642cc7b5cbe3f643a92d0ce54a7c957b6b
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-07-22 19:27:32 +0700

    feat: создать интерфейс для auth

---
+++ mobile/lib/features/auth/domain/interfaces/i_auth_repository.dart
@@ -0,0 +1,21 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/models/failures.dart';
+import '../../data/models/auth_response.dart';
+
+abstract class IAuthRepository {
+  // Either<Failure, AuthResponse>:
+  // Left — ошибка, Right — успешный ответ (с токенами и юзером)
+  Future<Either<Failure, AuthResponse>> login({
+    required String email,
+    required String password,
+  });
+
+  Future<Either<Failure, AuthResponse>> register({
+    required String email,
+    required String password,
+    required String username,
+  });
+
+  Future<Either<Failure, Unit>> logout();
+}
