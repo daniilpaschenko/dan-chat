d7f0b754eb31a8215ece585a0a842e36aa76b217
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-07-23 15:01:07 +0700

    feat: добавить роут /splash в paths

--- mobile/lib/core/navigation/route_paths.dart
+++ mobile/lib/core/navigation/route_paths.dart
@@ -5,6 +5,7 @@ class RoutePaths {
   static const String register = '/register';
   static const String chatList = '/chats';
   static const String chatRoom = '/chats/:roomId';
+  static const String splash = '/splash';
 
   static String chatRoomPath(String roomId) => '/chats/$roomId';
 }
