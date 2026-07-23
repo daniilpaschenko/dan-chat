688aa52b7599afb43c98b4bdc227f74329191a3c
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-07-23 15:37:34 +0700

    chore: заменить адрес на адрес андроид-эмулятора

--- mobile/lib/core/network/api_constants.dart
+++ mobile/lib/core/network/api_constants.dart
@@ -1,9 +1,10 @@
 class ApiConstants {
   ApiConstants._();
 
-  // TODO: поменять при переходе на тесты на мобилке
-  // для Android-эмулятора localhost бэкенда — это 10.0.2.2, а не localhost
-  static const String baseUrl = 'http://localhost:3000/api';
+  // static const String baseUrl = 'http://localhost:3000/api';
+
+  // для андроид-эмулятора
+  static const String baseUrl = 'http://10.0.2.2:3000/api';
 
   static const Duration connectTimeout = Duration(seconds: 10);
   static const Duration receiveTimeout = Duration(seconds: 10);
