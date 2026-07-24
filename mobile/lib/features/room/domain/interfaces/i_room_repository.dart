d77c461b6e90b9d7e011b34b47a8dc2ea0ebcf47
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-07-24 09:26:09 +0700

    feat: создать интерфейс для room repo

---
+++ mobile/lib/features/room/domain/interfaces/i_room_repository.dart
@@ -0,0 +1,28 @@
+import 'package:dartz/dartz.dart';
+import '../../../../core/models/failures.dart';
+import '../../data/models/room.dart';
+
+abstract class IRoomRepository {
+  Future<Either<Failure, List<RoomListItem>>> getMyRooms();
+
+  Future<Either<Failure, Room>> getRoomById(String roomId);
+
+  Future<Either<Failure, Room>> createRoom({
+    required RoomType type,
+    String? name,
+    String? avatarUrl,
+    required List<String> participantIds,
+  });
+
+  Future<Either<Failure, Unit>> markRoomAsRead(String roomId);
+
+  Future<Either<Failure, Room>> addParticipant({
+    required String roomId,
+    required String userId,
+  });
+
+  Future<Either<Failure, Room?>> removeParticipant({
+    required String roomId,
+    required String userId,
+  });
+}
