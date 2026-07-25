e0647b649fc56025f21c8d548ae85dff67f8c656
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-07-25 09:49:31 +0700

    feat: добавить /profile в paths

--- mobile/lib/core/navigation/route_paths.dart
+++ mobile/lib/core/navigation/route_paths.dart
@@ -1,11 +1,18 @@
 class RoutePaths {
   RoutePaths._();
 
+  // AUTH
   static const String login = '/login';
   static const String register = '/register';
+
+  // CHATS
   static const String chatList = '/chats';
   static const String chatRoom = '/chats/:roomId';
-  static const String splash = '/splash';
-
   static String chatRoomPath(String roomId) => '/chats/$roomId';
+  
+  // PROFILE
+  static const String profile = '/profile';
+
+  // ДРУГОЕ
+  static const String splash = '/splash';
 }
