4887829c6009920a8613c195f94a318ad218b6a9
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-07-27 17:56:50 +0700

    feat: зарегистрировать содержимое message/ в di-контейнере

--- mobile/lib/core/di/injection_container.dart
+++ mobile/lib/core/di/injection_container.dart
@@ -40,6 +40,14 @@ import '../../features/user/domain/interfaces/i_user_repository.dart';
 import '../../features/user/domain/usecases/search_users_usecase.dart';
 import '../../features/user/presentation/blocs/search/search_bloc.dart';
 
+// MESSAGE
+import '../../features/message/data/datasources/message_remote_datasource.dart';
+import '../../features/message/data/repositories/message_repository.dart';
+import '../../features/message/domain/interfaces/i_message_repository.dart';
+import '../../features/message/domain/usecases/get_room_messages_usecase.dart';
+import '../../features/message/domain/usecases/send_message_usecase.dart';
+import '../../features/message/presentation/blocs/chat_room_bloc.dart';
+
 final GetIt getIt = GetIt.instance;
 
 Future<void> setupDependencies() async {
@@ -165,5 +173,30 @@ Future<void> setupDependencies() async {
   // также нужен чистый Initial при заходе
   getIt.registerFactory<SearchBloc>(
     () => SearchBloc(searchUsersUseCase: getIt<SearchUsersUseCase>()),
+  );
+
+  // features/message/data
+  getIt.registerLazySingleton<MessageRemoteDatasource>(
+    () => MessageRemoteDatasource(getIt<Dio>()),
+  );
+  getIt.registerLazySingleton<IMessageRepository>(
+    () => MessageRepository(getIt<MessageRemoteDatasource>()),
+  );
+
+  // features/message/domain
+  getIt.registerLazySingleton<GetRoomMessagesUseCase>(
+    () => GetRoomMessagesUseCase(getIt<IMessageRepository>()),
+  );
+  getIt.registerLazySingleton<SendMessageUseCase>(
+    () => SendMessageUseCase(getIt<IMessageRepository>()),
+  );
+
+  // features/message/presentation
+  // также нужен чистый Initial при заходе
+  getIt.registerFactory<ChatRoomBloc>(
+    () => ChatRoomBloc(
+      getRoomMessagesUseCase: getIt<GetRoomMessagesUseCase>(),
+      sendMessageUseCase: getIt<SendMessageUseCase>(),
+    ),
   );
 }
