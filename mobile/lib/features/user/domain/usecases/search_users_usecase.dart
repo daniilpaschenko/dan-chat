163dcec40b3984defe7049821e8152f44d955798
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-07-27 08:50:29 +0700

    feat: создать search users usecase с проверкой на короткий запрос на клиенте

---
+++ mobile/lib/features/user/domain/usecases/search_users_usecase.dart
@@ -0,0 +1,21 @@
+import 'package:dartz/dartz.dart';
+import '../../../../core/errors/failures.dart';
+import '../../data/models/user_model.dart';
+import '../interfaces/i_user_repository.dart';
+
+class SearchUsersUseCase {
+  final IUserRepository _repository;
+
+  const SearchUsersUseCase(this._repository);
+
+  Future<Either<Failure, List<PartialUser>>> call(String query) {
+    // бэк требует минимум 3 символа: короткий запрос отсекаем на клиенте заранее,
+    // чтобы не слать его при каждом нажатии клавиши
+    final trimmed = query.trim();
+    if (trimmed.length < 3) {
+      return Future.value(const Right([]));
+    }
+
+    return _repository.searchUsers(trimmed);
+  }
+}
