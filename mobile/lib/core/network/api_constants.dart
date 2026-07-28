eb43adb1e6ea04f2282754e3a7c7db4c88995847
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-07-28 10:22:50 +0700

    chore: поменять baseUrl

--- mobile/lib/core/network/api_constants.dart
+++ mobile/lib/core/network/api_constants.dart
@@ -2,10 +2,10 @@ class ApiConstants {
   ApiConstants._();
   // для моего девайса
   // adb reverse tcp:3000 tcp:3000
-  static const String baseUrl = 'http://localhost:3000/api';
+  // static const String baseUrl = 'http://localhost:3000/api';
 
   // для андроид-эмулятора
-  // static const String baseUrl = 'http://10.0.2.2:3000/api';
+  static const String baseUrl = 'http://10.0.2.2:3000/api';
 
   static const Duration connectTimeout = Duration(seconds: 10);
   static const Duration receiveTimeout = Duration(seconds: 10);
