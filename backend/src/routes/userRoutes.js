4a5a22ccd1accf5a8f61385f9c004af8c50450d2
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-07-29 08:23:33 +0700

    feat(backend): подключить эндпоинт чужого профиля на роут users/:userId

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { getMe, uploadAvatar, searchUsers } = require('../controllers/userController');
+const { getMe, uploadAvatar, searchUsers, getUserById } = require('../controllers/userController');
 const authMiddleware = require('../middlewares/auth'); // для защищённых роутов
 const upload = require('../middlewares/upload');
 
@@ -7,6 +7,8 @@ router.get('/me', authMiddleware, getMe);
 // .single() потому что только 1 аватарку можно загрузить
 router.post('/me/avatar', authMiddleware, upload.single('avatar'), uploadAvatar);
 
+// обязательно search выше, чем :userId
 router.get('/search', authMiddleware, searchUsers);
+router.get('/:userId', authMiddleware, getUserById);
 
 module.exports = router;
