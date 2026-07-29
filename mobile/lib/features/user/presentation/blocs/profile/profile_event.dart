42fa4e517e83f69d9576a203a468d582c550fe40
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-07-29 21:57:45 +0700

    feat: добавить состояния и события в profile для createRoom()

--- mobile/lib/features/user/presentation/blocs/profile/profile_event.dart
+++ mobile/lib/features/user/presentation/blocs/profile/profile_event.dart
@@ -7,4 +7,7 @@ part 'profile_event.freezed.dart';
 class ProfileEvent with _$ProfileEvent {
   const factory ProfileEvent.started() = ProfileStarted;
   const factory ProfileEvent.avatarUploadRequested(File file) = ProfileAvatarUploadRequested;
+  const factory ProfileEvent.chatRequested() = ProfileChatRequested;
+  const factory ProfileEvent.chatNavigationHandled() = ProfileChatNavigationHandled;
+  const factory ProfileEvent.chatErrorHandled() = ProfileChatErrorHandled;
 }
