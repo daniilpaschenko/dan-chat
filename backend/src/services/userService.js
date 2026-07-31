ad1749b0e093194352a3e575b3af4cd95eded464
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-07-31 09:55:31 +0700

    feat: создать userService с функцией получения контаков пользователя. в будущем нужно использовать её для контаков и умного поиска. сейчас нужна для следующего коммита ->

---
+++ backend/src/services/userService.js
@@ -0,0 +1,15 @@
+const Room = require('../models/Room');
+
+// достаём id всех "контактов" юзера — тех, с кем у него есть общие комнаты
+async function getContactIds(userId) {
+    // все комнаты пользователя
+    const rooms = await Room.find({ 'participants.user': userId }).select('participants.user');
+    const ids = new Set(); // Set чтобы не было дубликатов
+    rooms.forEach((room) => { // перебираем все комнаты
+        room.participants.forEach((p) => { // и всех участников комнат
+            const id = p.user.toString(); // получаем айди каждого юзера
+            if (id !== userId) ids.add(id); // если айди != айди текущего юзера, то добавляем
+        });
+    });
+    return [...ids]; // преобразует сет обратно в массив
+}
