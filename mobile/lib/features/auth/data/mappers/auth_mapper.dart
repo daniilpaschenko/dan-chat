8207fcbcb72e5218315caf25f1967e08b4fc4e46
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-07-31 16:36:56 +0700

    feat: создать auth entity/mapper + freezed файл

---
+++ mobile/lib/features/auth/data/mappers/auth_mapper.dart
@@ -0,0 +1,14 @@
+import '../models/auth_response.dart';
+import '../../domain/entities/auth_entity.dart';
+
+// маппинг data-слой -> domain-слой
+// repository теперь отдаёт наружу AuthEntity, а не AuthResponse напрямую
+extension AuthResponseMapper on AuthResponse {
+  AuthEntity toEntity() {
+    return AuthEntity(
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+      user: user, // TODO: user.toEntity()
+    );
+  }
+}
