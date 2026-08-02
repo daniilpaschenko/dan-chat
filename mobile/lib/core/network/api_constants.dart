e72915d37fd0afcec6abae003eab35083c875850
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-08-02 14:03:34 +0700

    chore: поменять api constants для теста на 2 устройствах одновременно

--- mobile/lib/core/network/api_constants.dart
+++ mobile/lib/core/network/api_constants.dart
@@ -2,9 +2,13 @@ class ApiConstants {
   ApiConstants._();
   // для моего девайса
   // adb reverse tcp:3000 tcp:3000
-  static const String baseUrl = 'http://localhost:3000/api';
-  static const String socketUrl = 'http://localhost:3000';
+  //static const String baseUrl = 'http://localhost:3000/api';
+  //static const String socketUrl = 'http://localhost:3000';
   
+  // ДЛЯ УСТРОЙСТВ В МОЕЙ СЕТИ
+  static const String baseUrl = 'http://192.168.1.10:3000/api';
+  static const String socketUrl = 'http://192.168.1.10:3000';
+
   // для андроид-эмулятора
   // static const String baseUrl = 'http://10.0.2.2:3000/api';
   // без /api — socket.io висит на корне http-сервера
