14fc201bda95b8da97b36524afaae9c1c088181c
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-08-09 13:25:40 +0700

    feat: определять адрес исходя из устройства

--- frontend/lib/core/network/api_constants.dart
+++ frontend/lib/core/network/api_constants.dart
@@ -3,9 +3,10 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 class ApiConstants {
   ApiConstants._();
   
-  // ДЛЯ УСТРОЙСТВ В МОЕЙ СЕТИ
-  static const String baseUrl = 'http://192.168.1.10:3000/api';
-  static const String socketUrl = 'http://192.168.1.10:3000';
+  static String get baseUrl =>
+    kIsWeb ? 'http://localhost:3000/api' : 'http://192.168.1.10:3000/api';
+  static String get socketUrl =>
+    kIsWeb ? 'http://localhost:3000' : 'http://192.168.1.10:3000';
 
   static const Duration connectTimeout = Duration(seconds: 10);
   static const Duration receiveTimeout = Duration(seconds: 10);
