6efd7ce710ec21502370c579000e243a3e2124a3
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-08-16 12:13:25 +0700

    feat: создать LocalNotificationService

---
+++ frontend/lib/core/services/local_notification_service.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotificationService {
+  final _plugin = FlutterLocalNotificationsPlugin();
+
+  static const _channel = AndroidNotificationChannel(
+    'messages_channel', // id канала
+    'Сообщения', // видимое юзеру название в настройках Android
+    description: 'Уведомления о новых сообщениях в чате',
+    importance: Importance.high,
+  );
+
+  Future<void> init() async {
+    const androidInit = AndroidInitializationSettings('@mipmap/ic_launcher');
+    const initSettings = InitializationSettings(android: androidInit);
+
+    await _plugin.initialize(settings: initSettings);
+
+    await _plugin
+        .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(_channel);
+  }
+
+  Future<void> show({required String title, required String body}) async {
+    await _plugin.show(
+      id: DateTime.now().millisecondsSinceEpoch ~/ 1000, // уникальный id
+      title: title,
+      body: body,
+      notificationDetails: NotificationDetails(
+        android: AndroidNotificationDetails(
+          _channel.id,
+          _channel.name,
+          channelDescription: _channel.description,
+          importance: Importance.high,
+          priority: Priority.high,
+        ),
+      ),
+    );
+  }
+}
