e6ec092353bb21a5e19ccd5e40c93fc80eee14bc
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-08-21 09:57:14 +0700

    feat: создать юзкейс для uploadRoomAvatar

---
+++ frontend/lib/features/room/domain/usecases/upload_room_avatar_usecase.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+import 'package:dartz/dartz.dart';
+import '../../../../core/errors/failures.dart';
+import '../../domain/entities/room_entity.dart';
+import '../interfaces/i_room_repository.dart';
+
+class UploadRoomAvatarUseCase {
+  final IRoomRepository _repository;
+  const UploadRoomAvatarUseCase(this._repository);
+
+  Future<Either<Failure, RoomEntity>> call({
+    required String roomId,
+    required File file,
+  }) => _repository.uploadRoomAvatar(roomId: roomId, file: file);
+}
