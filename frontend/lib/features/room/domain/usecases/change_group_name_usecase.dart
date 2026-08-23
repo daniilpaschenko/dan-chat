25ee666e30410ba58222745570d4fc2937bec578
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-08-23 17:24:16 +0700

    feat: создать usecase для changeGroupName

---
+++ frontend/lib/features/room/domain/usecases/change_group_name_usecase.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import '../../../../core/errors/failures.dart';
+import '../interfaces/i_room_repository.dart';
+
+class ChangeGroupNameUsecase {
+  final IRoomRepository _repository;
+
+  const ChangeGroupNameUsecase(this._repository);
+
+  Future<Either<Failure, void>> call(String roomId, String name) {
+    return _repository.changeGroupName(roomId, name);
+  }
+}
