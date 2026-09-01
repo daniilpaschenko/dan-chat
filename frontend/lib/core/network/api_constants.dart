4b2a211e68f319e16303ba6e06f691a3020b9e6f
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-09-01 10:02:55 +0700

    feat: change apiurl to https://dan-mqz2.onrender.com

--- frontend/lib/core/network/api_constants.dart
+++ frontend/lib/core/network/api_constants.dart
@@ -2,15 +2,20 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 
 class ApiConstants {
   ApiConstants._();
-  
-  static String get baseUrl =>
-    kIsWeb ? 'http://localhost:3000/api' : 'http://10.241.97.197:3000/api';
-  static String get socketUrl =>
-    kIsWeb ? 'http://localhost:3000' : 'http://10.241.97.197:3000';
+
+  static const String _apiUrl = String.fromEnvironment(
+    'https://dan-mqz2.onrender.com',
+    defaultValue: 'http://localhost:3000',
+  );
+
+  static String get baseUrl => '$_apiUrl/api';
+
+  static String get socketUrl => _apiUrl;
 
   static const Duration connectTimeout = Duration(seconds: 10);
   static const Duration receiveTimeout = Duration(seconds: 10);
 
   static const String clientTypeHeader = 'X-Client-Type';
+
   static String get clientType => kIsWeb ? 'web' : 'mobile';
 }
